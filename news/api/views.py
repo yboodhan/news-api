af34e2da2f45f6c4589ded42be304bcc51ab36b0
yboodhan <yboodhan@gmail.com>
2020-03-17 15:24:51 -0700

    Created POST method for articles API

--- news/api/views.py
+++ news/api/views.py
@@ -6,9 +6,15 @@ from rest_framework.response import Response
 from news.models import Article
 from news.api.serializers import ArticleSerializer
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def article_list_create_api_view(request):
     if request.method == "GET":
         articles = Article.objects.filter(active=True)
-        serializer = ArticleSerializer(articles)
+        serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
