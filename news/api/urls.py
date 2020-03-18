76735ce7c0fbc58af9ed3f6184e966fe559950eb
yboodhan <yboodhan@gmail.com>
2020-03-18 12:08:35 -0700

    Created views using APIView class

--- news/api/urls.py
+++ news/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from news.api.views import article_list_create_api_view, article_detail_api_view
+# from news.api.views import article_list_create_api_view, article_detail_api_view
+from news.api.views import ArticleListCreateAPIView, ArticleDetailAPIView
 
 urlpatterns = [
-    path("articles/", article_list_create_api_view, name="article-list"),
-    path("articles/<int:pk>/", article_detail_api_view, name="article-detail")
+    # path("articles/", article_list_create_api_view, name="article-list"),
+    # path("articles/<int:pk>/", article_detail_api_view, name="article-detail")
+    path("articles/", ArticleListCreateAPIView.as_view(), name="article-list"),
+    path("articles/<int:pk>/", ArticleDetailAPIView.as_view(), name="article-detail")
 ]
