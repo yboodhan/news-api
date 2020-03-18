c291d825f939f577a6eb0e8a1b6f56c3c2b9b924
yboodhan <yboodhan@gmail.com>
2020-03-18 11:58:14 -0700

    Created all api views

--- news/api/urls.py
+++ news/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from news.api.views import article_list_create_api_view
+from news.api.views import article_list_create_api_view, article_detail_api_view
 
 urlpatterns = [
-    path("articles/", article_list_create_api_view, name="article-list")
+    path("articles/", article_list_create_api_view, name="article-list"),
+    path("articles/<int:pk>/", article_detail_api_view, name="article-detail")
 ]
--- news/api/views.py
+++ news/api/views.py
@@ -18,3 +18,26 @@ def article_list_create_api_view(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET", "PUT", "DELETE"])
+def article_detail_api_view(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return Response({"error": {
+            "code": 404,
+            "message": "Article not found!"
+        }}, status=status.HTTP_404_NOT_FOUND)
+    if request.method == "GET":
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
