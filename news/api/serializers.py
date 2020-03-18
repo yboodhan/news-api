6452ffde3881d91d0819d08cf4c1091b4a4fd8ae
yboodhan <yboodhan@gmail.com>
2020-03-18 12:22:00 -0700

    Created custom validators

--- news/api/serializers.py
+++ news/api/serializers.py
@@ -28,3 +28,16 @@ class ArticleSerializer(serializers.Serializer):
         instance.active = validated_data.get("active", instance.active)
         instance.save()
         return instance
+
+    def validate(self, data):
+        """ check that description and title are different """
+        if data["title"] == data["description"]:
+            raise serializers.ValidationError("Title and Description must be different from one another.")
+        return data
+
+    def validate_title(self, value):
+        if len(value) < 60:
+            raise serializers.ValidationError("Title must be at least 60 characters long.")
+        return value
+
+
